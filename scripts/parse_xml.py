de8803cc6574c0f85dda312237d47e7cd989b9fa
Sakura Anning Yoshihara <sakura@ibm.com>
2022-07-13 12:49:17 -0400

    extract impression and write to csv
    script to extract impression and write fn and mpression to csv file.

---
+++ scripts/parse_xml.py
@@ -0,0 +1,48 @@
+""" Extract impressions from each x-ray report xml file. """
+
+import xml.etree.ElementTree as et
+import os
+import csv
+
+
+def extract_impression(doc_root):
+    """ Extract only <AbstractText Label="IMPRESSION"> from xml document """
+
+    abstract = doc_root.find('MedlineCitation/Article/Abstract')
+
+    for abstract_text in abstract:
+        label = abstract_text.get('Label')
+        # print('label: ', label)
+        if label == "IMPRESSION":
+            return abstract_text.text
+
+
+# open the file and write the header
+with open('./data/impressions.csv', 'w', encoding='UTF8', newline='') as ff:
+    # create the csv writer
+    writer = csv.writer(ff)
+
+    # write the header
+    header = ['filename', 'impression']
+    writer.writerow(header)
+
+data_dir = "./data/xml_data/"
+for fn in os.listdir(data_dir):
+    f = data_dir + fn
+    # checking if it is a file
+    if os.path.isfile(f):
+        mytree = et.parse(f)
+        root = mytree.getroot()
+
+        # extract the impression from the xml file
+        impression = extract_impression(root)
+
+        # write the filename and impression to csv file
+        with open('./data/impressions.csv', 'a', encoding='UTF8', newline='') as ff:
+            # create the csv writer
+            writer = csv.writer(ff)
+
+            # write the data  to the csv file
+            data = [fn, impression]
+            # print(data)
+            writer.writerow(data)
