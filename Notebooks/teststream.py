767fb5f4a61437e4bb4b3971609d0cd8818ec267
abarnett1999 <87715968+abarnett1999@users.noreply.github.com>
2022-07-16 17:05:35 -0500

    updates

--- Notebooks/teststream.py
+++ Notebooks/teststream.py
@@ -2,23 +2,15 @@
 import streamlit as st
 import pandas as pd 
 from matplotlib import pyplot as plt
-from textblob import TextBlob
-from nltk.tokenize import sent_tokenize
-#from gensim.summarization.summarizer import summarize 
-#from gensim.summarization import keywords
-import spacy
-from spacy import displacy
-from collections import Counter
-import en_core_web_sm
-nlp = en_core_web_sm.load()
+
 
 
 #Headings for Web Application
-st.title("Natural Language Processing Web Application Example")
-st.subheader("What type of NLP service would you like to use?")
+st.title("Chest X-Rays-Normal or Abnormal")
+#st.subheader("What type of NLP service would you like to use?")
 
 #Picking what NLP task you want to do
-option = st.selectbox('NLP Service',('Sentiment Analysis', 'Entity Extraction', 'Text Summarization')) #option is stored in this variable
+#option = st.selectbox('NLP Service',('Sentiment Analysis', 'Entity Extraction', 'Text Summarization')) #option is stored in this variable
 
 #Textbox for text user is entering
 st.subheader("Enter the text you'd like to analyze.")
@@ -27,60 +19,23 @@ text = st.text_input('Enter text') #text is stored in this variable
 #Display results of the NLP task
 st.header("Results")
 
-#Function to take in dictionary of entities, type of entity, and returns specific entities of specific type
-def entRecognizer(entDict, typeEnt):
-    entList = [ent for ent in entDict if entDict[ent] == typeEnt]
-    return entList
+# @st.cache
+# def load_model():
+#     # model = path/to/model:object
+#     return model
 
+# def make_prediction(text_input, model):
+#     pred = model.predict(text_input)
+#     prob = model.predict_proba(text_input)
+#     return pred, prob
 
-#Sentiment Analysis
-if option == 'Sentiment Analysis':
+# model = load_model()
+# prediction, probability = make_prediction(text, model)
 
-    #Creating graph for sentiment across each sentence in the text inputted
-    sents = sent_tokenize(text)
-    entireText = TextBlob(text)
-    sentScores = []
-    for sent in sents:
-        text = TextBlob(sent)
-        score = text.sentiment[0]
-        sentScores.append(score)
+# put some streamlit displays for the prediction and probability
+# preds_text = {
+#     0 : "Normal",
+#     1 : "Refer to Radiologist"
+# }
 
-    #Plotting sentiment scores per sentencein line graph
-    st.line_chart(sentScores)
-
-    #Polarity and Subjectivity of the entire text inputted
-    sentimentTotal = entireText.sentiment
-    st.write("The sentiment of the overall text below.")
-    st.write(sentimentTotal)
-
-#Named Entity Recognition
-elif option == 'Entity Extraction':
-
-    #Getting Entity and type of Entity
-    entities = []
-    entityLabels = []
-    doc = nlp(text)
-    for ent in doc.ents:
-        entities.append(ent.text)
-        entityLabels.append(ent.label_)
-    entDict = dict(zip(entities, entityLabels)) #Creating dictionary with entity and entity types
-
-    #Using function to create lists of entities of each type
-    entOrg = entRecognizer(entDict, "ORG")
-    entCardinal = entRecognizer(entDict, "CARDINAL")
-    entPerson = entRecognizer(entDict, "PERSON")
-    entDate = entRecognizer(entDict, "DATE")
-    entGPE = entRecognizer(entDict, "GPE")
-
-    #Displaying entities of each type
-    st.write("Organization Entities: " + str(entOrg))
-    st.write("Cardinal Entities: " + str(entCardinal))
-    st.write("Personal Entities: " + str(entPerson))
-    st.write("Date Entities: " + str(entDate))
-    st.write("GPE Entities: " + str(entGPE))
-
-#Text Summarization
-else:
-    summWords = summarize(text)
-    st.subheader("Summary")
-    st.write(summWords)
+# st.text(f"The Results are: {preds_text[prediction]}")
