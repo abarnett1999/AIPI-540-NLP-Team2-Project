e1515805530cf0551771ecd514fd6fd6803cbd64
abarnett1999 <87715968+abarnett1999@users.noreply.github.com>
2022-07-18 17:26:33 -0500

    new

--- scripts/create_features.py
+++ scripts/create_features.py
@@ -1,21 +1,21 @@
-import os
 import numpy as np
 import pandas as pd
 import string
-import time
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from tqdm import tqdm
 from sklearn.linear_model import LogisticRegression
-import spacy
-from spacy.lang.en.stop_words import STOP_WORDS
-from spacy.lang.en import English
 import nltk
-from nltk.stem import WordNetLemmatizer
 import warnings
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import recall_score
 
-nlp = spacy.load('en_core_web_sm')
+# importing sys
+import sys
+
+# adding Folder_2 to the system path
+from scripts import make_dataset, model, text_preprocessing, create_features
+
+sys.path.insert(0, 'scripts')
+
+
 nltk.download('omw-1.4')
 nltk.download('wordnet')
 warnings.filterwarnings('ignore')
