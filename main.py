4a38818edabe54e9a40c3e447e9926658ee31510
Sakura Anning Yoshihara <sakuraya10@gmail.com>
2022-07-18 17:40:15 -0400

    fix filepaths in main

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ import nltk
 import warnings
 from sklearn.metrics import recall_score
 
-from scripts/make_dataset import make_dataset
-from scripts/text_preprocessing import tokenize
-from scripts/model import train_model
-from scripts/create_features import build_features
+# importing sys
+import sys
+
+# adding Folder_2 to the system path
+from scripts import make_dataset, model, text_preprocessing, create_features
+
+sys.path.insert(0, 'scripts')
+
 
 nlp = spacy.load('en_core_web_sm')
 nltk.download('omw-1.4')
@@ -21,26 +25,26 @@ nltk.download('wordnet')
 warnings.filterwarnings('ignore')
 
 """ Get data """
-X_train, X_test, y_train, y_test = make_dataset()
+X_train, X_test, y_train, y_test = make_dataset.make_dataset()
 
 
 """ Tokenize and lemmatize the data """
 # Process the training set
 tqdm.pandas()
-X_train_processed = X_train.progress_apply(lambda x: tokenize(x, method='nltk'))
+X_train_processed = X_train.progress_apply(lambda x: text_preprocessing.tokenize(x, method='nltk'))
 
 # Process the test set text
 tqdm.pandas()
-X_test_processed = X_test.progress_apply(lambda x: tokenize(x, method='nltk'))
+X_test_processed = X_test.progress_apply(lambda x: text_preprocessing.tokenize(x, method='nltk'))
 
 """ Create features using  Count Vectorization """
 method = 'count'
 ngram_range = (1, 2)
-X_train, X_test = build_features(X_train_processed, X_test_processed, ngram_range, method)
+X_train, X_test = create_features.build_features(X_train_processed, X_test_processed, ngram_range, method)
 
 
 """ Train model on training set """
-logreg_model = train_model(X_train, y_train)
+logreg_model = model.train_model(X_train, y_train)
 preds = logreg_model.predict(X_train)
 acc = sum(preds == y_train) / len(y_train)
 recall = recall_score(y_train, preds)
